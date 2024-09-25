1eff5ea4bbaab9847c4fc2b13869152a1389b895
AndrewGWMHS <goff.614615@swl.k12.oh.us>
2024-09-25 08:18:46 -0400

    final

--- src/NumCompare.java
+++ src/NumCompare.java
@@ -1,5 +1,38 @@
-public class Main {
+import java.util.Scanner;
+
+public class NumCompare {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int num1;
+        int num2;
+        System.out.println("Enter your first integer:");
+        if (scan.hasNextInt()) {
+            num1 = scan.nextInt();
+            System.out.println("Enter your second integer:");
+            if (scan.hasNextInt()) {
+                num2 = scan.nextInt();
+                if (num1 > num2) {
+                    System.out.println(num1 + " is greater than " + num2 + ".");
+                }
+                else if (num1 < num2) {
+                    System.out.println(num1 + " is less than " + num2 + ".");
+                }
+                else {
+                    System.out.println(num1 + " is equal to " + num2 + ".");
+                }
+            }
+            else
+            {
+                System.out.println("Invalid data type");
+                System.exit(0);
+            }
+        }
+        else
+        {
+            System.out.println("Invalid data type");
+            System.exit(0);
+        }
+
+
     }
 }
